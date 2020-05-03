49ddd82951b71dba713cb99f14c2d600e6731e90
lizf <lizf@zetyun.com>
2020-05-03 17:06:13 +0800

    add code

---
+++ src/main/java/com/phigey/yurika/list/CollectionsMain.java
@@ -0,0 +1,12 @@
+package com.phigey.yurika.list;
+
+import java.util.Collections;
+
+/**
+ * @author cfa
+ */
+public class CollectionsMain {
+    public static void main(String[] args) {
+        Collections.emptyList();
+    }
+}
