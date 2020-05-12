9f2d603e9fd27bb3ac6952032fe2ead526983498
lizf <lizf@zetyun.com>
2020-05-12 18:48:25 +0800

    add some code and remove some code

---
+++ src/main/java/com/phigey/yurika/hadoop/HadoopMain.java
@@ -0,0 +1,30 @@
+package com.phigey.yurika.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * @author cfa
+ */
+public class HadoopMain {
+    public static void main(String[] args) {
+        Configuration configuration = HDFSUtils.initConfiguration();
+
+        HDFSUtils.initKerberosENV(configuration);
+
+        try {
+            Path dstPath = new Path("/user");
+            FileSystem fileSystem = FileSystem.get(configuration);
+            FileStatus[] listStatus = fileSystem.listStatus(dstPath);
+            for (FileStatus fileStatus : listStatus) {
+                Path path = fileStatus.getPath();
+                System.out.println(path);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
